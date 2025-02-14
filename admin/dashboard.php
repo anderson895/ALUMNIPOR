c64acf715101840e9092162cb11f8f2050bc4cd7
JARP <andersonandy046@gmail.com>
2025-02-14 21:30:17 +0800

    admin

---
+++ admin/dashboard.php
@@ -0,0 +1,46 @@
+<?php include "components/header.php";?>
+<link rel="stylesheet" href="css/style.css">
+<!-- Top bar with user profile -->
+<div class="flex justify-between items-center bg-white p-4 mb-6 rounded-md shadow-md">
+    <h2 class="text-lg font-semibold text-gray-700">Dashboard</h2>
+    <div class="w-10 h-10 bg-gray-300 rounded-full flex items-center justify-center text-lg font-bold text-white">
+        <?php
+        echo substr(ucfirst($result[0]['admin_username']), 0, 1);
+        ?>
+    </div>
+</div>
+
+<!-- Dashboard Cards -->
+<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6">
+    <!-- Card for Total Customer -->
+    <div class="bg-white p-6 rounded-lg shadow-lg flex flex-col items-center">
+        <img src="assets/campus.png" alt="students icon" class="mb-4 w-12 max-w-full" />
+        <h3 class="text-gray-700 font-semibold text-lg">Total Campus</h3>
+        <p class="text-blue-500 text-2xl font-bold count_users">0</p>
+    </div>
+
+    <!-- Card for Total Sales -->
+    <div class="bg-white p-6 rounded-lg shadow-lg flex flex-col items-center">
+        <img src="assets/school.png" alt="students icon" class="mb-4 w-12 max-w-full" />
+        <h3 class="text-gray-700 font-semibold text-lg">Total Alumni</h3>
+        <p class="text-blue-500 text-2xl font-bold totalSales">0</p>
+    </div>
+
+    
+</div>
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+<?php include "components/footer.php";?>
